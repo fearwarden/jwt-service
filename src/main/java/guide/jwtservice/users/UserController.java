7edd2c777c5c0a700c39cd808472d79eed48eaed
Boris Antonijev <antonijevboris@gmail.com>
2023-08-13 14:07:21 +0200

    user model

---
+++ src/main/java/guide/jwtservice/users/UserController.java
@@ -0,0 +1,7 @@
+package guide.jwtservice.users;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/guide/jwtservice/users/enums/Role.java
@@ -0,0 +1,6 @@
+package guide.jwtservice.users.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
