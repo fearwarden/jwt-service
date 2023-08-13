ad13935710553005602ed4be32708de750509231
Boris Antonijev <antonijevboris@gmail.com>
2023-08-13 22:53:35 +0200

    Update UserController.java

--- src/main/java/guide/jwtservice/users/UserController.java
+++ src/main/java/guide/jwtservice/users/UserController.java
@@ -1,7 +1,16 @@
 package guide.jwtservice.users;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @GetMapping()
+    public String test() {
+        return "Prosao";
+    }
+
 }
