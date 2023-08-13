167a6d3a2fb4d5ee3d0b48ea093575f14ef7a093
Boris Antonijev <antonijevboris@gmail.com>
2023-08-13 14:17:01 +0200

    user repository

---
+++ src/main/java/guide/jwtservice/users/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package guide.jwtservice.users.repositories;
+
+import guide.jwtservice.users.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
